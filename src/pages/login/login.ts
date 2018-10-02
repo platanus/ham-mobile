abf55511cdab20f72c3dfe5e6ae3d9540f3b7b6c
fdominguezc <fdominguezc@uc.cl>
2018-10-02 19:10:14 -0300

    feat(): market tab

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -81,6 +81,7 @@ export class LoginPage {
           this.navCtrl.push(HomePage)
         },
         err => {
+          console.log(err)
           const errorMessage = JSON.parse(err.error).message
           this.displayError(errorMessage)
         }
