d5954239d2d8303f57e53a203d20234873a8e847
Guillermo Moreno <gmq@guillermomoreno.cl>
2018-10-18 20:27:04 -0300

    feat(app): use store for login navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,35 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Store } from '@ngrx/store';
 
 import { LoginPage } from '../pages/login/login';
+import * as fromRoot from '../store';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  public rootPage: any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      statusBar.hide();
-      splashScreen.hide();
+  constructor(
+    private platform: Platform,
+    private statusBar: StatusBar,
+    private splashScreen: SplashScreen,
+    private store: Store<fromRoot.AppState>,
+  ) {
+    this.platform.ready().then(() => {
+      this.statusBar.hide();
+      this.splashScreen.hide();
+
+      this.store.select(fromRoot.getAuthHamCode).subscribe((hamCode) => {
+        if (hamCode) {
+          this.rootPage = TabsPage;
+        } else {
+          this.rootPage = LoginPage;
+        }
+      });
     });
   }
 }
